7e59af4dae098ce1ff19045147b6c60aa870eac3
lcai000 <landoncai777@gmail.com>
2025-09-25 20:47:24 -0500

    Initial Commit

---
+++ matchaShop/matchaShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  matchaShopApp.swift
+//  matchaShop
+//
+//  Created by Landon Cai on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct matchaShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
